3d617416d5e0b8f6d84953fffe9831c74c8df1e4
kalmarek <kalmar@amu.edu.pl>
2021-01-07 15:13:06 +0100

    add W-statistic

--- src/ShapiroWilk.jl
+++ src/ShapiroWilk.jl
@@ -15,4 +15,18 @@ include("royston.jl")
 include("normordstats_nemo.jl")
 include("normordstats_arblib.jl")
 
+function Wstatistic(X, A::SWCoeffs)
+
+    @assert issorted(X)
+
+    if last(X) - first(X) < length(X)*eps(eltype(X))
+        throw("Data seems to be constant!")
+    end
+
+    AX = dot(A,X)
+    S² = sum(abs2, X.-mean(X))
+
+    return AX^2/S²
+end
+
 end # of module ShapiroWilk
